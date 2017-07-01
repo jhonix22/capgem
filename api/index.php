aa666a3559bd6f05a75228561ed0dcdfa72d9cef
jhonix22 <roljhon1991@gmail.com>
2017-07-01 18:56:39 +0800

    updates

--- api/index.php
+++ api/index.php
@@ -1,3 +1,39 @@
 <?php
 
+    require 'config/config.php'; //database connection
+
+    $paramCode = $_REQUEST['paramCode']; //paramCode variable
+
+    //initialize response array
+    $response = new array(
+                            "success" => 0, //1 = successed or 0 = failed
+                            "message" => "Initial Message" //response message
+                         );
+
+    if(!empty($_REQUEST)){
+        if(!empty($paramCode)){
+            switch($paramCode){
+                
+                /* LOGIN */
+                case 'login':
+                    $username = $_REQUEST['username']; //get username param
+                    $password = $_REQUEST['password']; //get password param
+
+                    /* QUERY DATABASE TO CHECK IF USER EXIST */
+
+
+                break;
+
+                /* NOTIFICATION */
+                case 'notification':
+                    
+                break;
+
+                default:
+                    $response = $response;
+            }
+        }
+    }
+
+    echo json_encode($response);
 ?>
