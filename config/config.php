490a1783b8c29fb462f93cbf72b8befe78900245
Roljhon Teano <roljhon1991@gmail.com>
2017-07-04 04:05:31 +0800

    updated config for localhost mysql connection

--- config/config.php
+++ config/config.php
@@ -1,5 +1,9 @@
 <?php
-      $mysqli = new mysqli("us-cdbr-iron-east-03.cleardb.net", "b2c92c9b237584", "2f16bc0f", "heroku_306f40779193461");
+      /* ClearDB MySQL Connection */
+      //$mysqli = new mysqli("us-cdbr-iron-east-03.cleardb.net", "b2c92c9b237584", "2f16bc0f", "heroku_306f40779193461");
+      
+      /* LOCALHOST MySQL Connection*/
+      $mysqli = new mysqli("localhost", "root", "", "capgem");
       if ($mysqli->connect_errno) {
           echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
       }
