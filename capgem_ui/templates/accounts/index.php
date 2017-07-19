16a04a63646c72b33558652d04e624a30e3acc43
Milky Jogue <milky.dude11@gmail.com>
2017-07-19 16:33:16 +0800

    Accounts added 07192017
    ari na pre oh.. gin tingob ko nalang na.. sorry gid now kolang na ubra sako2 gid kobi.

---
+++ capgem_ui/templates/accounts/index.php
@@ -0,0 +1,42 @@
+<div class="profile-content">
+	<div class="col-md-12">
+		<div class="col-md-4 pending-list">
+			<h3 class="">List of Pending Members</h3>
+			<table class="table-members">
+				<tr>
+					<td>Noel Aldwin Yanes</td>
+					<td><a href="#"><div class="glyphicon glyphicon-ok-sign approve"></div></a><a href="#"><div class="glyphicon glyphicon-remove-sign remove"></div></a></td>
+				</tr>
+				<tr>
+					<td>Clariville Malunes</td>
+					<td><a href="#"><div class="glyphicon glyphicon-ok-sign approve"></div></a><a href="#"><div class="glyphicon glyphicon-remove-sign remove"></div></a></td>
+				</tr>
+				<tr>
+					<td>Audrey Lachica</td>
+					<td><a href="#"><div class="glyphicon glyphicon-ok-sign approve"></div></a><a href="#"><div class="glyphicon glyphicon-remove-sign remove"></div></a></td>
+				</tr>
+				<tr>
+					<td>Maeve Dulaca</td>
+					<td><a href="#"><div class="glyphicon glyphicon-ok-sign approve"></div></a><a href="#"><div class="glyphicon glyphicon-remove-sign remove"></div></a></td>
+				</tr>
+			</table>
+		</div>
+		<div class="col-md-5">
+			<h1 class="req-title">Requirements</h1>
+			<div class="req-content-container">
+				<div class="personal-information">
+					<label>Personal Information</label>
+				</div>
+				<div class="loan-information">
+					<label>Loan Information</label>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="action-container">
+				<button class="btn-approved"><div class="glyphicon glyphicon-thumbs-up"></div> APPROVED</button>
+				<button class="btn-rejected"><div class="glyphicon glyphicon-thumbs-down"></div> REJECTED</button>
+			</div>
+		</div>
+	</div>
+</div>
