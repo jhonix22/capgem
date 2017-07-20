9534a0e947b56a24b23e43e130dd334a5fac657a
jhonix22 <roljhon1991@gmail.com>
2017-07-21 01:11:07 +0800

    header.php update

---
+++ capgem_ui/templates/header.php
@@ -0,0 +1,71 @@
+<!-- Sidebar -->
+        <div id="sidebar-wrapper">
+			<div class="logo">
+				<a href="#">
+					<img src="assets/img/logo.jpg"/>
+				</a>
+			</div>
+            <ul class="sidebar-nav">
+                <li>
+                    <a href="<?php echo $url->site_url('');?>" class="<?= ($p == '') ? 'active':''; ?>">Home</a>
+                </li>
+                <li>
+                     <?php
+                        if(!empty($_SESSION['user_id'])){
+                            ?>
+                                <a href="<?php echo $url->site_url('index.php?p=profile&user_id=');?>" class="<?= ($p == 'profile') ? 'active':''; ?>">Profile</a>
+                            <?php
+                        }
+                    ?> 
+                    
+                </li>
+                <li>
+                    <a href="<?php echo $url->site_url('index.php?p=members');?>" class="<?= ($p == 'members') ? 'active':''; ?>">Members</a>
+                </li>
+                <li>
+                    <a href="<?php echo $url->site_url('index.php?p=employees');?>" class="<?= ($p == 'employees') ? 'active':''; ?>">Employees</a>
+                </li>
+                <li>
+                    <a href="<?php echo $url->site_url('index.php?p=calendar');?>" class="<?= ($p == 'calendar') ? 'active':''; ?>">Calendar</a>
+                </li>
+                <li>
+                    <a href="<?php echo $url->site_url('index.php?p=reports');?>" class="<?= ($p == 'reports') ? 'active':''; ?>">Reports</a>
+                </li>
+                <li>
+                    <a href="<?php echo $url->site_url('index.php?p=contacts');?>" class="<?= ($p == 'contacts') ? 'active':''; ?>">Contacts</a>
+                </li>
+                <li>
+                    <a href="<?php echo $url->site_url('index.php?p=apply');?>" class="<?= ($p == 'apply') ? 'active':''; ?>">Apply Now</a>
+                </li>
+                <li>
+                    <?php
+                        if(!empty($_SESSION['user_id'])){
+                            ?>
+                                <a href="javascript:void(0);" class="">Log Out</a>
+                            <?php
+                        }
+                    ?> 
+                </li>
+            </ul>
+			<div class="copyright">Capgem &copy; Copyright <?php echo date("Y");?></div>
+        </div>
+        <!-- /#sidebar-wrapper -->
+		<header>
+			<div class="topbar">
+			<?php
+				if($p != ''){
+			?>
+				<div class="user-label">
+					<span>Welcome Admin</span>
+				</div>
+			<?php
+				}
+			?>
+				<input type="text" placeholder="Search">
+				<div class="<?= ($p == 'accounts') ? 'message-container-active':''; ?> message-container">
+					<a href="<?php echo $url->site_url('index.php?p=accounts');?>">
+						<div class="message glyphicon glyphicon-envelope"></div>
+					</a>
+				</div>
+			</div>
+		</header>
