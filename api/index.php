0181296c5c096259e2c55abc63dc1b31f946103d
Roljhon Teano <roljhon1991@gmail.com>
2017-08-01 05:21:08 +0800

    android integration update

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
 
-    require '../config/config.php'; //database connection
+     require '../myPHPLibraries/controller/connection.php';
+     require '../includes/functions.php';
 
     $paramCode = isset($_REQUEST['paramCode']); //paramCode variable
 
@@ -20,8 +21,28 @@
                     $password = $_REQUEST['password']; //get password param
 
                     /* QUERY DATABASE TO CHECK IF USER EXIST */
+                     $col = ['user_id','access','verified'];
+                     $con = array('username' => $username, 'password'=> $password);
 
+                     $result = $obj->select("tbl_user", $col, $con);
 
+                        if(!empty($result)){
+                            foreach($result[0] as $key => $value):
+                                $response['res'][0][$key] = $value;
+                            endforeach;
+                             $response['success'] = 1;
+                        }else{
+                             $response['success'] = 0;
+                             $response['message'] = 'User not Found!';
+                        }
+
+                        if(!empty($response['res'][0]['user_id']) 
+                            && ($response['res'][0]['access'] == 2) 
+                            && ($response['res'][0]['verified'] == 0) )
+                        {
+                            $response['success'] = 2;
+                            $response['message'] = 'User not verified. Please check your email upon registration!';
+                        }
                 break;
 
                 /* NOTIFICATION */
